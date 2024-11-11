678268b1ce59be9d1d778662db9fb1a3e41814a5
Alena Tim <musiamilka35@gmail.com>
2024-11-11 13:22:24 +0300

    ДЗ по третьему уроку

---
+++ homework3.py
@@ -0,0 +1,66 @@
+# 1
+# number = -1.6
+# number2 = 2.99
+# print(int(number))
+# print(int(number2))
+
+# задание 2
+# line = "www.my_site.com#about"
+# new_line = line.replace('#','/')
+# print(new_line)
+
+# задание 3
+# roster = "stroka"
+# roster2 = "ing"
+# result = roster + roster2
+# print (result)
+
+# задание 4
+# rename = "Ivanou Ivan"
+# print(type(rename))
+# rename2 = rename.replace("Ivanou Ivan", "Ivan Ivanou ", 1)
+# print(rename2)
+
+# задание 5
+# book = " war and peace "
+# print(book.strip(" "))
+
+# задание 6
+# school = {
+#     "1a" : 10,
+#     "1b" : 11,
+#     "1c" : 12,
+#     "1d" : 13,
+#     "1f" : 10,
+#     "2a" : 11,
+#     "2b" : 12,
+#     "2c" : 13,
+#     "2d" : 14,
+#     "2e" : 15
+# }
+# print(school)
+# print(school ["2b"])
+
+# задание 7
+# numbers = [1, 1, 3, 5]
+# numbers2 = list(range(1, 11))
+# print(numbers2)
+# print(13 in numbers2)
+
+# задание 8
+# activite = "employ, employment"
+# print(activite.count("employ"))
+
+# задание 9
+# x = "My name is Agent Smith"
+# print(x[1])
+# print(x[3:16:3])
+
+
+
+
+
+
+
+
+
